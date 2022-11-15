a8f704012c0769969ade321da1b7824dc6ea8c54
zxc58 <abc01234567889@gmail.com>
2022-11-15 09:09:04 +0800

    aws deploy

--- config/session.js
+++ config/session.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 const logger = require('../helpers/winston')
 //
+const expireTime = 86400 * 1000
 const sessionSecret = process.env.SESSION_SECRET
 const redisClient = redis.createClient({
   url: process.env.REDIS_CONNECT_STRING || 'redis://localhost:6379',
@@ -12,7 +13,7 @@ redisClient.on('connect', () => { logger.info('Redis client connected') })
 redisClient.on('error', () => { logger.error('redis on error') })
 redisClient.connect()
 const sessionConfiguration = session({
-  cookie: { maxAge: 86400000 },
+  cookie: { maxAge: expireTime },
   store: new RedisStore({ client: redisClient }),
   secret: sessionSecret,
   resave: false,
