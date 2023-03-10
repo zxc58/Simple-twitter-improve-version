aeabc4fcffd133a55740bc4312c46572c924de31
Gotolas <abc01234567889@gmail.com>
2023-03-10 10:40:57 +0800

    cancel redis

--- config/session.js
+++ config/session.js
@@ -1,23 +1,24 @@
-const redis = require('redis')
+// const redis = require('redis')
 const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
-const logger = require('../helpers/winston')
+// const RedisStore = require('connect-redis')(session)
+// const logger = require('../helpers/winston')
 //
-const expireTime = 86400 * 1000
+// const expireTime = 86400 * 1000
 const sessionSecret = process.env.SESSION_SECRET
-const redisClient = redis.createClient({
-  url: process.env.REDIS_CONNECT_STRING || 'redis://localhost:6379',
-  legacyMode: true
-})
-redisClient.on('connect', () => { logger.info('Redis client connected') })
-redisClient.on('error', () => { logger.error('redis on error') })
-redisClient.connect()
-const sessionConfiguration = session({
-  cookie: { maxAge: expireTime },
-  store: new RedisStore({ client: redisClient }),
-  secret: sessionSecret,
-  resave: false,
-  saveUninitialized: false
-})
+// const redisClient = redis.createClient({
+//   url: process.env.REDIS_CONNECT_STRING || 'redis://localhost:6379',
+//   legacyMode: true
+// })
+// redisClient.on('connect', () => { logger.info('Redis client connected') })
+// redisClient.on('error', () => { logger.error('redis on error') })
+// redisClient.connect()
+// const sessionConfiguration = session({
+//   cookie: { maxAge: expireTime },
+//   store: new RedisStore({ client: redisClient }),
+//   secret: sessionSecret,
+//   resave: false,
+//   saveUninitialized: false
+// })
 //
+const sessionConfiguration = session({ secret: sessionSecret, resave: false, saveUninitialized: false })
 module.exports = sessionConfiguration
